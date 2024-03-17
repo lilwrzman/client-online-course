36b783ecca26ace357a8de8f10c7d429e54addd3
egydyahermawan <egy20ti@mahasiswa.pcr.ac.id>
2024-03-18 01:12:53 +0700

    feat: add navbar component default state

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,16 @@
+<header>
+	<nav class="container flex justify-content-between align-items-center">
+		<div class="h5">Logo Disini</div>
+		<ul class="navbar-menu">
+			<li class="navbar-menu-item"><a href="">Beranda</a></li>
+			<li class="navbar-menu-item"><a href="">Alur Belajar</a></li>
+			<li class="navbar-menu-item"><a href="">Materi</a></li>
+			<li class="navbar-menu-item"><a href="">Event</a></li>
+			<li class="navbar-menu-item"><a href="">Artikel</a></li>
+		</ul>
+		<div class="flex gap-2 align-items-center">
+			<button class="btn btn-primary-outline">Masuk</button>
+			<button class="btn btn-primary">Daftar</button>
+		</div>
+	</nav>
+</header>
