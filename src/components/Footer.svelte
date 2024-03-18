2880201a18624878706ef95ae6684a60c4ebb1e2
egydyahermawan <egy20ti@mahasiswa.pcr.ac.id>
2024-03-18 23:05:35 +0700

    feat: add footer and input field component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,55 @@
+<script>
+
+</script>
+
+<footer>
+    <div class="container">
+        <hr>
+        <div class="flex-column gap-6 my-6">
+            <div class="flex justify-content-between">
+                <div class="flex-column gap-4">
+                    <div class="h5">Logo Disini</div>
+                    <div class="body-medium-reguler" style="max-width: 250px;">
+                        Lorem ipsum dolor sit amet consectetur adipiscing elit Nunc tincidunt finibus tortor.
+                    </div>
+                </div>
+                <div class="flex-column gap-4">
+                    <div class="body-super-large-semi-bold">Lorem Ipsum</div>
+                    <div class="flex-column gap-3">
+                        <a href="" class="body-small-medium">Lorem Ipsum</a>
+                        <a href="" class="body-small-medium">Lorem Ipsum</a>
+                        <a href="" class="body-small-medium">Lorem Ipsum</a>
+                        <a href="" class="body-small-medium">Lorem Ipsum</a>
+                    </div>
+                </div>
+                <div class="flex-column gap-4">
+                    <div class="body-super-large-semi-bold">Lorem Ipsum</div>
+                    <div class="flex-column gap-3">
+                        <a href="" class="body-small-medium">Lorem Ipsum</a>
+                        <a href="" class="body-small-medium">Lorem Ipsum</a>
+                        <a href="" class="body-small-medium">Lorem Ipsum</a>
+                        <a href="" class="body-small-medium">Lorem Ipsum</a>
+                    </div>
+                </div>
+                <div class="flex gap-4 align-items-center">
+                    <a href=""><img src="/icons/Instagram.svg" alt="icon"></a>
+                    <a href=""><img src="/icons/Facebook.svg" alt="icon"></a>
+                    <a href=""><img src="/icons/Twitter.svg" alt="icon"></a>
+                </div>
+            </div>
+            <div class="body-small-reguler">
+                © Lorem ipsum dolor sit amet.
+            </div>
+        </div>
+    </div>
+</footer>
+
+<style>
+    footer > div > hr{
+        border: 1px solid var(--neutral-border);
+    }
+
+    footer a{
+        color: #718284;
+    }
+</style>
---
+++ src/components/InputField.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let type = "text"
+    export let label = ""
+    export let id = ""
+    export let name = ""
+    export let placeholder = ""
+
+    let showPassword = false
+
+    let toggleVisibility = () => {
+        showPassword = !showPassword
+    }
+</script>
+
+<div class="flex-column gap-2">
+    <label for="{id}" class="body-large-reguler">{label}</label>
+    {#if type == 'password'}
+    <div class="input-group-container">
+        <input type="{showPassword ? 'text' : 'password'}" id="{id}" name="{name}" placeholder="{placeholder}">
+        <div class="password-icon">
+            <img src="/icons/{showPassword ? 'eye' : 'eye-slash'}.svg" alt="icon" on:click={toggleVisibility}>
+        </div>
+    </div>
+    {:else}
+    <input type="{type}" id="{id}" name="{name}" placeholder="{placeholder}">
+    {/if}
+</div>
+
+<style>
+    .password-icon{
+        position: absolute;
+        top: 50%;
+        right: 14px;
+        transform: translateY(-50%);
+        cursor: pointer;
+    }
+</style>
