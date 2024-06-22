488eeaf17df5b14f3aaae4ffc8f00793228352f4
egydyahermawan <egy20ti@mahasiswa.pcr.ac.id>
2024-06-22 18:37:02 +0700

    error page part 1

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script>
+    import {page} from '$app/stores'
+    import Button from '@components/Button.svelte';
+	import { onMount } from 'svelte';
+
+    onMount(() => {
+        console.log($page)
+        console.error($page.error)
+    })
+</script>
+
+{#if $page.status == '404'}
+<h1>{$page.status} Error cuy!</h1>
+<p>{$page.error.message}</p>
+<Button classList="btn btn-main-outline" onClick={() => history.back()}>Kembali</Button>
+{/if}
+
+<svelte:head>
+    <title>Oops!</title>
+</svelte:head>
