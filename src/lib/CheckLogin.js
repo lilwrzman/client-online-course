d6fab73bcb2e8fc1d58d687bb6734de5311ee699
egydyahermawan <egy20ti@mahasiswa.pcr.ac.id>
2024-06-22 18:39:04 +0700

    fixing module

--- src/lib/CheckLogin.js
+++ src/lib/CheckLogin.js
@@ -5,12 +5,12 @@ const checkLogin = (role = null) => {
     let user = extract('datas')
         
     if(!user){
-        return setFlash({ message: "Harap login terlebih dahulu!", type: 'danger', redirect: '/' })
+        return setFlash({ title: 'Oops', message: "Harap login terlebih dahulu!", type: 'danger', redirect: '/' })
     }
 
     if(role && user.role != role){
         destroyCookie('datas')
-        return setFlash({ message: "Anda tidak berwenang, silahkan login ulang dengan akun yang sesuai!", type: 'danger', redirect: '/' })
+        return setFlash({ title: 'Oops', message: "Anda tidak berwenang, silahkan login ulang dengan akun yang sesuai!", type: 'warning', redirect: '/' })
     }
 
     return user
--- src/lib/Flash.js
+++ src/lib/Flash.js
@@ -1,24 +1,27 @@
 import { goto } from "$app/navigation"
 
 const setFlash = (flash) => {
+    sessionStorage.setItem('flashTitle', flash.title)
     sessionStorage.setItem('flashMessage', flash.message)
     sessionStorage.setItem('flashType', flash.type)
 
-    goto(flash.redirect)
+    return goto(flash.redirect)
 }
 
 const getFlash = () => {
+    let title = sessionStorage.getItem('flashTitle')
     let message = sessionStorage.getItem('flashMessage')
     let type = sessionStorage.getItem('flashType')
 
-    if(!message || !type){
+    if(!message || !type || !title){
         return null
     }
 
+    sessionStorage.removeItem('flashTitle');
     sessionStorage.removeItem('flashMessage');
     sessionStorage.removeItem('flashType');
 
-    return { message, type }
+    return { title, message, type }
 }
 
 export { setFlash, getFlash }
