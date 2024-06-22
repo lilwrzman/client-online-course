456bf3fd235cd0b2b5e6601867f49e497ef0bbf6
egydyahermawan <egy20ti@mahasiswa.pcr.ac.id>
2024-06-22 18:43:59 +0700

    fixing superadmin dashboard

--- src/routes/superadmin/dashboard/+page.svelte
+++ src/routes/superadmin/dashboard/+page.svelte
@@ -1,39 +1,67 @@
 <script>
     import { onMount } from "svelte";
-    import { goto } from '$app/navigation'
 
-    import { extract } from "$lib/Cookie.js"
 	import { getCurrentDay, getCurrentTime } from "$lib/Date";
+	import ApiController from "$lib/ApiController";
+	import checkLogin from "$lib/CheckLogin";
+	import { getFlash } from "$lib/Flash";
 	
     import Navbar from "@components/Navbar.svelte";
     import Sidebar from "@components/Sidebar.svelte";
+    import Toast from "@components/Toast.svelte";
     import { Hourglass } from "svelte-bootstrap-icons";
 
     let user = null
+    let dashboard = null
+    let status = false
+    let toastData = null
+    let toastVisible = false
 
-    let ratingReviewData = null
+    const getDashboard = () => {
+        ApiController.sendRequest({
+            endpoint: 'dashboard',
+            method: 'GET',
+            authToken: user.token
+        }).then(response => {
+            if(response.status){
+                dashboard = response.data
+                status = true
+            }
+        })
+    }
 
     onMount(() => {
-        user = extract('datas')
-        
-        if(!user){
-            goto('/superadmin/login')
+        user = checkLogin('Superadmin')
+
+        let flashes = getFlash()
+        if(flashes){
+            toastData = {
+                title: flashes.title,
+                message: flashes.message,
+                color: `toast-${flashes.type}`
+            }
+            toastVisible = true
         }
+
+        getDashboard()
     })
 </script>
 
 <div class="flex">
-    <Sidebar active="Dashboard"/>
+    <Sidebar active="Dashboard" role="Superadmin"/>
     <div class="neutral-wrapper px-3">
-        <Navbar active="" variant="inside" pageTitle="Dashboard"/>
+        <Navbar active="" variant="inside" pageTitle="Dashboard" bind:user={user}/>
         <main>
             <div class="container flex-column py-4 gap-8">
+                {#if toastVisible}
+                    <Toast bind:toastVisible title={toastData.title} message={toastData.message} color={toastData.color}/>
+                {/if}
                 <div class="card px-4 pt-4 pb-0 drop-shadow">
                     <div class="card-body gap-5">
                         <div class="flex justify-content-between">
                             <div class="flex-column">
-                                <div class="body-small-semi-bold">Welcome to SuperAdmin Area</div>
-                                <div class="caption-light">All documents here are confidential.</div>
+                                <div class="body-small-semi-bold">Selamat datang di panel Admin LPK</div>
+                                <div class="caption-light">Semua dokumen di sini bersifat rahasia.</div>
                             </div>
                             <div class="flex-column">
                                 <div class="caption-reguler">{ getCurrentDay() }</div>
@@ -44,29 +72,29 @@
                             <div class="flex-column align-items-center px-2 flex-item-md-3 flex-item-sm-6 flex-item-xs-12 mb-4">
                                 <img src="/icons/dashboard-total-student.svg" alt="sum-student-icon" width="30">
                                 <div class="flex-column align-items-center py-2">
-                                    <div class="caption-reguler">999</div>
-                                    <div class="caption-light">Total Student</div>
+                                    <div class="caption-reguler">{status ? dashboard.count_student : 0}</div>
+                                    <div class="caption-light">Total Karyawan</div>
                                 </div>
                             </div>
                             <div class="flex-column align-items-center px-2 flex-item-md-3 flex-item-sm-6 flex-item-xs-12 mb-4">
                                 <img src="/icons/dashboard-total-content.svg" alt="sum-content-icon" width="30">
                                 <div class="flex-column align-items-center py-2">
-                                    <div class="caption-reguler">999</div>
-                                    <div class="caption-light">Total Content</div>
+                                    <div class="caption-reguler">{status ? dashboard.count_course : 0}</div>
+                                    <div class="caption-light">Total Materi</div>
                                 </div>
                             </div>
                             <div class="flex-column align-items-center px-2 flex-item-md-3 flex-item-sm-6 flex-item-xs-12 mb-4">
                                 <img src="/icons/dashboard-total-corporate.svg" alt="sum-corporate-icon" width="30">
                                 <div class="flex-column align-items-center py-2">
-                                    <div class="caption-reguler">999</div>
-                                    <div class="caption-light">Total Corporate</div>
+                                    <div class="caption-reguler">{status ? dashboard.count_corporate : 0}</div>
+                                    <div class="caption-light">Total Mitra</div>
                                 </div>
                             </div>
                             <div class="flex-column align-items-center px-2 flex-item-md-3 flex-item-sm-6 flex-item-xs-12 mb-4">
                                 <img src="/icons/dashboard-total-transaction.svg" alt="sum-transaction-icon" width="30">
                                 <div class="flex-column align-items-center py-2">
-                                    <div class="caption-reguler">999</div>
-                                    <div class="caption-light">Total Transaction</div>
+                                    <div class="caption-reguler">{status ? dashboard.count_transaction : 0}</div>
+                                    <div class="caption-light">Total Transaksi</div>
                                 </div>
                             </div>
                         </div>
@@ -76,50 +104,39 @@
                 <div class="card p-4 drop-shadow">
                     <div class="card-body gap-5">
                         <div class="flex justify-content-between">
-                            <div class="body-small-semi-bold">Latest Rating & Review</div>
-                            <a href="/" class="link caption-reguler-thin">View All</a>
+                            <div class="body-small-semi-bold">Transaksi Terbaru</div>
+                            <a href="/" class="link caption-reguler-thin">Lihat Semua</a>
                         </div>
-                        {#if ratingReviewData}
-                        <div class="flex-column gap-3">
-                            <div class="row px-4 rating-review-item">
-                                <div class="col-2 flex-column justify-content-center">
-                                    <img src="/images/TestimoniSampleImage.svg" alt="">
-                                </div>
-                                <div class="col-10">
-                                    <div class="flex-column justify-content-center h-100">
-                                        <div class="body-small-semi-bold">Lorem Ipsum</div>
-                                        <div class="caption-semi-light">Body standard phasellus justo purus, vene natis a sapien eu faucibus porttitor libero. Sapieds tortor, nec vulputate sem efficitur. standard phasellus justo purus, vene natis a sapien eu faucibus porttitor libero</div>
+                        {#if status}
+                            {#if dashboard.transaction_list.length > 0}
+                                {#each dashboard.transaction_list as transaction }
+                                <div class="flex-column gap-3">
+                                    <div class="row px-4 rating-review-item">
+                                        <div class="col-2 flex-column justify-content-center">
+                                            <img src="/images/TestimoniSampleImage.svg" alt="">
+                                        </div>
+                                        <div class="col-10">
+                                            <div class="flex-column justify-content-center h-100">
+                                                <div class="body-small-semi-bold">Lorem Ipsum</div>
+                                                <div class="caption-semi-light">Body standard phasellus justo purus, vene natis a sapien eu faucibus porttitor libero. Sapieds tortor, nec vulputate sem efficitur. standard phasellus justo purus, vene natis a sapien eu faucibus porttitor libero</div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="row px-4 rating-review-item">
-                                <div class="col-2 flex-column justify-content-center">
-                                    <img src="/images/TestimoniSampleImage.svg" alt="">
-                                </div>
-                                <div class="col-10">
-                                    <div class="flex-column justify-content-center h-100">
-                                        <div class="body-small-semi-bold">Lorem Ipsum</div>
-                                        <div class="caption-semi-light">Body standard phasellus justo purus, vene natis a sapien eu faucibus porttitor libero. Sapieds tortor, nec vulputate sem efficitur. standard phasellus justo purus, vene natis a sapien eu faucibus porttitor libero</div>
-                                    </div>
+                                {/each}
+                            {:else}
+                            <div class="flex justify-content-center align-items-center w-100 py-8">
+                                <div class="flex-column align-items-center gap-3 py-8">
+                                    <Hourglass width=60 height=60 color="#3951A8"/>
+                                    <div class="caption-reguler tc-neutral-disabled">Ups, belum ada data transaksi!</div>
                                 </div>
                             </div>
-                            <div class="row px-4 rating-review-item">
-                                <div class="col-2 flex-column justify-content-center">
-                                    <img src="/images/TestimoniSampleImage.svg" alt="">
-                                </div>
-                                <div class="col-10">
-                                    <div class="flex-column justify-content-center h-100">
-                                        <div class="body-small-semi-bold">Lorem Ipsum</div>
-                                        <div class="caption-semi-light">Body standard phasellus justo purus, vene natis a sapien eu faucibus porttitor libero. Sapieds tortor, nec vulputate sem efficitur. standard phasellus justo purus, vene natis a sapien eu faucibus porttitor libero</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                            {/if}
                         {:else}
                         <div class="flex justify-content-center align-items-center w-100 py-8">
                             <div class="flex-column align-items-center gap-3 py-8">
                                 <Hourglass width=60 height=60 color="#3951A8"/>
-                                <div class="caption-reguler tc-neutral-disabled">Ups, you don’t have any ratings or review yet</div>
+                                <div class="caption-reguler tc-neutral-disabled">Ups, belum ada data transaksi!</div>
                             </div>
                         </div>
                         {/if}
@@ -129,6 +146,10 @@
         </main>
     </div>
 </div>
+
+<svelte:head>
+    <title>Beranda</title>
+</svelte:head>
 
 <style>
     .rating-review-item img{
