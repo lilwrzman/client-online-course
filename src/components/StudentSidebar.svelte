4cd3f640ec588e2aaca59c185b175f1f81c91c98
egydyahermawan <egy20ti@mahasiswa.pcr.ac.id>
2024-06-25 15:24:32 +0700

    sidebar student and my courses page

---
+++ src/components/StudentSidebar.svelte
@@ -0,0 +1,122 @@
+<script>
+    import { BarChartFill, GearFill, HouseDoorFill, TicketFill } from "svelte-bootstrap-icons";
+
+    export let user
+    export let active = 'Kursus Saya'
+</script>
+
+<aside>
+    <div class="sidebar neutral-border radius-sm">
+        <div class="flex-column p-3" style="gap: 14px;">
+            <img src="{user ? `http://127.0.0.1:8000/storage/${user.avatar}` : '/images/default.png'}" alt="user-avatar" width="90" height="90">
+            <div class="flex-column">
+                <p class="body-large-semi-bold">{ user ? user.fullname : ''}</p>
+                <p class="body-medium-reguler">{ user ? 'Karyawan' : ''}</p>
+            </div>
+            <ul class="sidebar-menu">
+                <li>
+                    <a href="/student/my-courses"
+                        class="menu {active == 'Kursus Saya' ? 'menu-active' : ''}" >
+                        <HouseDoorFill width="20" height="20" color={active == 'Kursus Saya' ? '#3951A8' : '#8191AC'} />
+                        <p class="body-small-semi-bold {active == 'Kursus Saya' ? 'tc-primary-main' : 'tc-neutral-primary'}" >
+                            Kursus Saya
+                        </p>
+                    </a>
+                </li>
+                <li>
+                    <a href="/student/progress"
+                        class="menu {active == 'Progress Belajar' ? 'menu-active' : ''}" >
+                        <BarChartFill width="20" height="20" color={active == 'Progress Belajar' ? '#3951A8' : '#8191AC'} />
+                        <p class="body-small-semi-bold {active == 'Progress Belajar' ? 'tc-primary-main' : 'tc-neutral-primary'}" >
+                            Progress Belajar
+                        </p>
+                    </a>
+                </li>
+                <li>
+                    <a href="/student/redeem"
+                        class="menu {active == 'Tukar Kode' ? 'menu-active' : ''}" >
+                        <TicketFill width="20" height="20" color={active == 'Tukar Kode' ? '#3951A8' : '#8191AC'} />
+                        <p class="body-small-semi-bold {active == 'Tukar Kode' ? 'tc-primary-main' : 'tc-neutral-primary'}" >
+                            Tukar Kode
+                        </p>
+                    </a>
+                </li>
+                <li>
+                    <a href="/student/settings"
+                        class="menu {active == 'Pengaturan Akun' ? 'menu-active' : ''}" >
+                        <GearFill width="20" height="20" color={active == 'Pengaturan Akun' ? '#3951A8' : '#8191AC'} />
+                        <p class="body-small-semi-bold {active == 'Pengaturan Akun' ? 'tc-primary-main' : 'tc-neutral-primary'}" >
+                            Pengaturan Akun
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</aside>
+
+<style>
+    aside {
+		background-color: var(--neutral-white);
+		display: flex;
+		width: 17.5rem;
+        height: fit-content;
+		position: sticky;
+		position: -webkit-sticky;
+		top: 0;
+	}
+
+	aside > div {
+		overflow-y: auto;
+	}
+
+    .sidebar {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+		justify-content: space-between;
+	}
+
+    .sidebar-menu {
+		padding: 0;
+		margin: 0;
+		list-style: none;
+		display: flex;
+		flex-direction: column;
+		gap: 14px;
+	}
+
+	.menu {
+		font-family: 'Poppins', sans-serif;
+		display: flex;
+		gap: 24px;
+		padding: 20px 14px;
+		align-items: center;
+		border-radius: 0.25rem;
+		cursor: pointer;
+		border: none;
+		width: 100%;
+		background-color: transparent;
+		transition: all 0.25s ease;
+	}
+
+	.menu:hover {
+		background-color: var(--hover);
+		padding: 1.375rem 0.875rem;
+	}
+
+	.menu-dropdown-item {
+		padding: 20px 14px;
+		transition: all 0.25s ease;
+		border-radius: 0.25rem;
+	}
+
+	.menu-dropdown-item:hover {
+		background-color: var(--hover);
+		padding: 1.375rem 0.875rem;
+	}
+
+	.menu-active {
+		background-color: var(--hover);
+	}
+</style>
---
+++ src/routes/student/my-courses/+page.svelte
@@ -0,0 +1,160 @@
+<script>
+    import { onMount } from "svelte";
+	import checkLogin from "$lib/CheckLogin";
+    import Navbar from "@components/Navbar.svelte";
+    import StudentSidebar from "@components/StudentSidebar.svelte";
+	import Tab from "@components/Tab.svelte";
+    import Button from "@components/Button.svelte";
+
+    let user, active = "Semua Materi"
+
+    onMount(() => {
+        user = checkLogin('Student', true)
+    })
+
+</script>
+
+<Navbar/>
+
+{#if user}
+<section class="section" id="my-courses">
+    <div class="container">
+        <div class="flex gap-4">
+            <StudentSidebar bind:user={user}/>
+            <main class="w-100">
+                <div class="container">
+                    <div class="flex-column gap-standard">
+                        <h4>Kursus Saya</h4>
+                        
+                        <Tab bind:value={active} type="variable" menus={[
+                            { value: "Semua Materi", title: "Semua Materi" },
+                            { value: "Selesai", title: "Selesai" }
+                        ]}/>
+
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6 col-md-3 mb-4">
+                                <div class="card neutral-border radius-sm">
+                                    <div class="card-body gap-3">
+                                        <img src="/images/building-trust-image.png" class="radius-sm" alt="gambar courses" />
+                                        <div class="flex-column gap-1">
+                                            <p class="body-small-medium label-bullet">Sales Marketing</p>
+                                            <p class="body-small-medium">Building Trust</p>
+                                            <div class="flex justify-content-between align-items-center"> 
+                                                <p class="caption-small-reguler">Rp. 150.000</p>
+                                                <p class="caption-small-reguler">20 Item</p>                         
+                                            </div>                          
+                                        </div>
+                                        <Button type="link" href="/courses/1" classList="btn btn-main">Lihat Detail</Button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xs-12 col-sm-6 col-md-3">
+                                <div class="card neutral-border radius-sm">
+                                    <div class="card-body gap-3">
+                                        <img src="/images/building-trust-image.png" class="radius-sm" alt="gambar courses" />
+                                        <div class="flex-column gap-1">
+                                            <p class="body-small-medium label-bullet">Sales Marketing</p>
+                                            <p class="body-small-medium">Building Trust</p>
+                                            <div class="flex justify-content-between align-items-center"> 
+                                                <p class="caption-small-reguler">Rp. 150.000</p>
+                                                <p class="caption-small-reguler">20 Item</p>                         
+                                            </div>                          
+                                        </div>
+                                        <Button type="link" href="/courses/1" classList="btn btn-main">Lihat Detail</Button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xs-12 col-sm-6 col-md-3">
+                                <div class="card neutral-border radius-sm">
+                                    <div class="card-body gap-3">
+                                        <img src="/images/building-trust-image.png" class="radius-sm" alt="gambar courses" />
+                                        <div class="flex-column gap-1">
+                                            <p class="body-small-medium label-bullet">Sales Marketing</p>
+                                            <p class="body-small-medium">Building Trust</p>
+                                            <div class="flex justify-content-between align-items-center"> 
+                                                <p class="caption-small-reguler">Rp. 150.000</p>
+                                                <p class="caption-small-reguler">20 Item</p>                         
+                                            </div>                          
+                                        </div>
+                                        <Button type="link" href="/courses/1" classList="btn btn-main">Lihat Detail</Button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xs-12 col-sm-6 col-md-3">
+                                <div class="card neutral-border radius-sm">
+                                    <div class="card-body gap-3">
+                                        <img src="/images/building-trust-image.png" class="radius-sm" alt="gambar courses" />
+                                        <div class="flex-column gap-1">
+                                            <p class="body-small-medium label-bullet">Sales Marketing</p>
+                                            <p class="body-small-medium">Building Trust</p>
+                                            <div class="flex justify-content-between align-items-center"> 
+                                                <p class="caption-small-reguler">Rp. 150.000</p>
+                                                <p class="caption-small-reguler">20 Item</p>                         
+                                            </div>                          
+                                        </div>
+                                        <Button type="link" href="/courses/1" classList="btn btn-main">Lihat Detail</Button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xs-12 col-sm-6 col-md-3">
+                                <div class="card neutral-border radius-sm">
+                                    <div class="card-body gap-3">
+                                        <img src="/images/building-trust-image.png" class="radius-sm" alt="gambar courses" />
+                                        <div class="flex-column gap-1">
+                                            <p class="body-small-medium label-bullet">Sales Marketing</p>
+                                            <p class="body-small-medium">Building Trust</p>
+                                            <div class="flex justify-content-between align-items-center"> 
+                                                <p class="caption-small-reguler">Rp. 150.000</p>
+                                                <p class="caption-small-reguler">20 Item</p>                         
+                                            </div>                          
+                                        </div>
+                                        <Button type="link" href="/courses/1" classList="btn btn-main">Lihat Detail</Button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xs-12 col-sm-6 col-md-3">
+                                <div class="card neutral-border radius-sm">
+                                    <div class="card-body gap-3">
+                                        <img src="/images/building-trust-image.png" class="radius-sm" alt="gambar courses" />
+                                        <div class="flex-column gap-1">
+                                            <p class="body-small-medium label-bullet">Sales Marketing</p>
+                                            <p class="body-small-medium">Building Trust</p>
+                                            <div class="flex justify-content-between align-items-center"> 
+                                                <p class="caption-small-reguler">Rp. 150.000</p>
+                                                <p class="caption-small-reguler">20 Item</p>                         
+                                            </div>                          
+                                        </div>
+                                        <Button type="link" href="/courses/1" classList="btn btn-main">Lihat Detail</Button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xs-12 col-sm-6 col-md-3">
+                                <div class="card neutral-border radius-sm">
+                                    <div class="card-body gap-3">
+                                        <img src="/images/building-trust-image.png" class="radius-sm" alt="gambar courses" />
+                                        <div class="flex-column gap-1">
+                                            <p class="body-small-medium label-bullet">Sales Marketing</p>
+                                            <p class="body-small-medium">Building Trust</p>
+                                            <div class="flex justify-content-between align-items-center"> 
+                                                <p class="caption-small-reguler">Rp. 150.000</p>
+                                                <p class="caption-small-reguler">20 Item</p>                         
+                                            </div>                          
+                                        </div>
+                                        <Button type="link" href="/courses/1" classList="btn btn-main">Lihat Detail</Button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+</section>
+{/if}
